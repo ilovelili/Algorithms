2bfaedb49940d6d55af9d96c657937d2021a4301
ilovelili <route666@live.cn>
2016-11-26 23:42:45 +0900

    change gitignore and add wrapper

---
+++ algorithm.go
@@ -0,0 +1,16 @@
+package algorithm
+
+import (
+	"algorithmutil"
+
+	"./sort"
+)
+
+var (
+	sorter *sort.Sorter
+)
+
+// BubbleSort BubbleSort entry
+func BubbleSort(arrayzor []algorithmutil.IComparable) []algorithmutil.IComparable {
+	return sorter.BubbleSort(arrayzor)
+}
